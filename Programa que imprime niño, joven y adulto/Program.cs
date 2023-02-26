5e7c90eb23e4a0b1cf42c153a231ce99bb33b0fb
LENOVO <LENOVO@LAPTOP-U952235S>
2023-02-25 22:03:03 -0800

    Agregar archivos de proyecto.

---
+++ Programa que imprime niño, joven y adulto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa_que_imprime_niño__joven_y_adulto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese tu edad");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad < 13)
+            {
+                Console.WriteLine("Eres un niño");
+
+            }
+
+            else if (edad <= 25)
+            {
+                Console.WriteLine("Eres joven");
+            }
+
+            else
+            {
+                Console.WriteLine("Eres un adulto");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Promedio de N notas/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio_de_N_notas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
